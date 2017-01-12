4f0e9f3a45dc1f66665b831d065da646e82f934c
Afonso Arantes <afonso@locomobi.com>
2017-01-12 10:55:29 -0500

    [CSRAPP-10] Removed obsolete comment

--- app/models/violation.rb
+++ app/models/violation.rb
@@ -4,9 +4,6 @@ class Violation < ApplicationRecord
 
   belongs_to :employee, optional: false
 
-  # todo: enforce this in the database
   validates :violation_date, :violation_amount, :violation_number, :payment_status, presence: true
-
-
  
 end
